c560e4904e5242cc7fa2792934c3fb062299169d
Fulvio Corno <fulvio.corno@polito.ikt>
2017-04-06 19:04:39 +0200

    Developed in classroom

---
+++ python-web.py
@@ -0,0 +1,32 @@
+from flask import Flask, url_for, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = "MUVETSOssIHYJGeeNM3"
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/secret.html', methods=['POST'])
+def hello():
+    username = request.form['username']
+    value = len(username)
+
+    session['username'] = username
+    session['value'] = value
+
+    if value==0:
+        return render_template("error.html")
+    else:
+        return render_template("secret.html",
+                           username=username, secret_value=value)
+
+
+@app.route("/secret_data.html")
+def data():
+    secretdata = [ 1, 1, 2, 3, 5, 8, 13, 21 ] # FAKE DATA -- should be user dependent
+    return render_template("data.html", username=session['username'], secretdata=secretdata)
+
+if __name__ == '__main__':
+    app.run(debug=True)
